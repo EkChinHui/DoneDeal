2ab702b748915f25e624c16c0e5990ed23bf439c
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-01-17 22:45:57 +0800

    added addexpenditure activity and sqlite functionality

---
+++ app/src/main/java/com/nus/donedeal/AddExpenditureActivity.java
@@ -0,0 +1,13 @@
+package com.nus.donedeal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddExpenditureActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addexpenditurelayout);
+    }
+
+}
---
+++ app/src/main/java/com/nus/donedeal/DatabaseHelper1.java
@@ -0,0 +1,55 @@
+package com.nus.donedeal;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper1 extends SQLiteOpenHelper {
+    private static final String TAG = "DatabaseHelper";
+    private static final String TABLE_NAME = "Expenditure_table";
+    public static final String COL1 = "Description";
+    public static final String COL2 = "Price";
+    public static final String COL3 = "Paid by";
+
+    public DatabaseHelper1(Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1 + " TEXT," + COL2 + " REAL, " + COL3 + "TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i , int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, item);
+
+        Log.d(TAG, "addData: Adding " + item + " to" + TABLE_NAME);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        // if date is inserted incorrectly it will return -1
+        if (result == -1) return false;
+        else return true;
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+}
