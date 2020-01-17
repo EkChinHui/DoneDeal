b93faa8e430362afa083865e0833360e9b1a337b
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-01-17 16:18:52 +0800

    +main activity layout enter button +go to home page after clicking enter button +activities in xml file

--- app/src/main/java/com/nus/donedeal/MainActivity.java
+++ app/src/main/java/com/nus/donedeal/MainActivity.java
@@ -1,12 +1,24 @@
 package com.nus.donedeal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Button btn_enter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlayout);
+        btn_enter = findViewById(R.id.btn_enter);
+        btn_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
