fa8b5c8a1df6eb6df43881419f0344d9043c8a60
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-01-17 15:15:07 +0800

    ok done

--- app/src/main/java/com/nus/donedeal/MainActivity.java
+++ app/src/main/java/com/nus/donedeal/MainActivity.java
@@ -1,5 +1,12 @@
 package com.nus.donedeal;
 
-public class MainActivity {
-    hi
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
