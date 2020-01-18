86c6a738ca46cd16875f7d191481b65b38d03f06
songyu <songyu98@gmail.com>
2020-01-18 20:29:09 +0800

    reset button

--- app/src/main/java/com/nus/donedeal/MainActivity.java
+++ app/src/main/java/com/nus/donedeal/MainActivity.java
@@ -10,34 +10,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
+
 public class MainActivity extends Activity {
-    Button btn_enter, btn_addexpenditure;
+    Button btn_enter, btn_reset;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (getStatus() == 0) {
+            btn_reset.setVisibility(View.GONE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlayout);
         btn_enter = findViewById(R.id.btn_enter);
+        btn_reset = findViewById(R.id.btnreset);
+        if (getStatus() == 0) {
+            btn_reset.setVisibility(View.GONE);
+        }
+
         btn_enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (getStatus() == 1) {
                     Intent intent = new Intent(getApplicationContext(), AddExpenditureActivity.class);
                     startActivity(intent);
-                }
-                else {
+                } else {
                     Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                     startActivity(intent);
                 }
             }
         });
 
-        btn_addexpenditure = findViewById(R.id.btnaddexpenditure);
-        btn_addexpenditure.setOnClickListener(new View.OnClickListener() {
+        btn_reset.setOnClickListener(new View.OnClickListener() {
             @Override
-            public  void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AddExpenditureActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                setStatus();
+                Toast.makeText(MainActivity.this, "Trip Completed", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -46,5 +60,12 @@ public class MainActivity extends Activity {
         SharedPreferences sharedPreferences = getSharedPreferences("Pref", Context.MODE_PRIVATE);
         Integer status = sharedPreferences.getInt("Status", 0);
         return status;
+    }
+
+    private void setStatus() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("Status", 0);
+        editor.apply();
     }
 }
