b1b973c08ec777f1103beb35cf74d2a0ad63d68f
wenhao <wenhaogoh1998@gmail.com>
2020-01-19 10:30:04 +0800

    ui

--- app/src/main/java/com/nus/donedeal/MainActivity.java
+++ app/src/main/java/com/nus/donedeal/MainActivity.java
@@ -1,6 +1,5 @@
 package com.nus.donedeal;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,7 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
     Button btn_enter, btn_show, btn_reset;
     TextView tripName;
     DatabaseHelper databaseHelper;
